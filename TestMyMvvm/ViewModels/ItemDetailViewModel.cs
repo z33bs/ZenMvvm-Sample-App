611ce1fc5a1b2d976b01ac71446d4ffbb1edb38d
Zeebs <zeebsmail@gmail.com>
2020-09-14 10:10:47 +0200

    Initial

---
+++ TestMyMvvm/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using TestMyMvvm.Models;
+
+namespace TestMyMvvm.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
